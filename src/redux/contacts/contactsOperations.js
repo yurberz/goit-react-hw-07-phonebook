e507a87ceeada6120de5e0573fa4e474fdd2cb78
Yurii Berezynets <yuriiberezynets@gmail.com>
2021-02-05 07:10:41 +0200

    fix env.process

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,44 +1,7 @@
 import axios from "axios";
 import contactsActions from "./contactsActions";
 
-// axios.defaults.baseURL = "http://localhost:2000";
-
-// const getContacts = () => async (dispatch) => {
-//   dispatch(contactsActions.getContactsRequest());
-
-//   try {
-//     const { data } = await axios.get("/contacts");
-//     dispatch(contactsActions.getContactsSuccess(data));
-//   } catch (error) {
-//     dispatch(contactsActions.getContactsError(error));
-//   }
-// };
-
-// const addContact = (payload) => async (dispatch) => {
-//   dispatch(contactsActions.addContactRequest());
-
-//   try {
-//     const { data } = await axios.post("/contacts", { ...payload });
-//     dispatch(contactsActions.addContactSuccess(data));
-//   } catch (error) {
-//     dispatch(contactsActions.addContactError(error));
-//   }
-// };
-
-// const delContact = (id) => async (dispatch) => {
-//   dispatch(contactsActions.delContactRequest());
-
-//   try {
-//     await axios.delete(`/contacts/${id}`);
-//     dispatch(contactsActions.delContactSuccess(id));
-//   } catch (error) {
-//     dispatch(contactsActions.delContactError(error));
-//   }
-// };
-
-// export default { getContacts, addContact, delContact };
-
-axios.defaults.baseURL = `${process.env.REACT_APP_BASE_URL}`;
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 const getContacts = () => async (dispatch) => {
   dispatch(contactsActions.getContactsRequest());
@@ -78,3 +41,43 @@ const delContact = (id) => async (dispatch) => {
 };
 
 export default { getContacts, addContact, delContact };
+
+/*
+json server
+*/
+// axios.defaults.baseURL = "http://localhost:2000";
+
+// const getContacts = () => async (dispatch) => {
+//   dispatch(contactsActions.getContactsRequest());
+
+//   try {
+//     const { data } = await axios.get("/contacts");
+//     dispatch(contactsActions.getContactsSuccess(data));
+//   } catch (error) {
+//     dispatch(contactsActions.getContactsError(error));
+//   }
+// };
+
+// const addContact = (payload) => async (dispatch) => {
+//   dispatch(contactsActions.addContactRequest());
+
+//   try {
+//     const { data } = await axios.post("/contacts", { ...payload });
+//     dispatch(contactsActions.addContactSuccess(data));
+//   } catch (error) {
+//     dispatch(contactsActions.addContactError(error));
+//   }
+// };
+
+// const delContact = (id) => async (dispatch) => {
+//   dispatch(contactsActions.delContactRequest());
+
+//   try {
+//     await axios.delete(`/contacts/${id}`);
+//     dispatch(contactsActions.delContactSuccess(id));
+//   } catch (error) {
+//     dispatch(contactsActions.delContactError(error));
+//   }
+// };
+
+// export default { getContacts, addContact, delContact };
